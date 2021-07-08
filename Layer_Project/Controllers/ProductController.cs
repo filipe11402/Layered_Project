23850cb9bda71c9de35f15858e3ae33c74910e59
Filipe Encarnacao <filipe11402@outlook.pt>
2021-07-08 20:34:35 +0100

    Repository Pattern for Infrastructure layer

---
+++ Layer_Project/Controllers/ProductController.cs
@@ -0,0 +1,29 @@
+﻿using Domain.Entities;
+using Infrastructure.Context;
+using Microsoft.AspNetCore.Mvc;
+using Project.Domain.Repositories;
+using Project.Infrastructure.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Application.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository _productRepository;
+        private readonly ApplicationDbContext _productDb;
+
+        public ProductController()
+        {
+            _productRepository = new ProductRepository(_productDb);
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Product> productList = _productRepository.GetProducts();
+            return View(productList);
+        }
+    }
+}
